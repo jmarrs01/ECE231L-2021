cf7767473337a207f789f970b86b044d5ec6a872
jmarrs01 <jmarrs01@unm.edu>
2021-08-27 11:43:27 -0600

    Fixing git after practice using service

--- vectortests.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <vector>
-
-void scalar_multiply(double scalar, std::vector<double> &d_vector);
-
-double vector_dot_product(std::vector<double> first_vector,
-	std::vector<double> second_vector);
